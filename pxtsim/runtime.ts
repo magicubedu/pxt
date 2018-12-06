f6f2522d3b408c6034c23478eeab8f5552f92aa0
Franklin Tse <FranklinWhale@users.noreply.github.com>
2018-12-06 13:36:15 +0800

    Remove debugger

--- pxtsim/runtime.ts
+++ pxtsim/runtime.ts
@@ -41,7 +41,6 @@ namespace pxsim {
 
         export function assert(cond: boolean, msg = "Assertion failed") {
             if (!cond) {
-                debugger
                 throw new Error(msg)
             }
         }
