6b60dd634e3647b3662cb31722a77cc78483e477
Franklin Tse <FranklinWhale@users.noreply.github.com>
2025-03-06 16:42:36 +0800

    Fix import

--- localtypings/dom.d.ts
+++ localtypings/dom.d.ts
@@ -2,10 +2,13 @@ interface Navigator {
     msSaveOrOpenBlob(blob: any, defaultName?: string): boolean;
 }
 
+type BlockContent = {
+    ts?: string;
+    blocks?: string;
+};
+
 interface Window {
     blockCopyHandler(data: any): void;
-    blockPasteHandler(fn: (content: {
-        ts?: string;
-        blocks?: string;
-    }) => void): void;
+    blockPasteHandler(fn: (content: BlockContent) => void): void;
+    importBlocks(content: BlockContent): void;
 }
